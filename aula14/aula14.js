1d80f66f8db3b119eeb708cc71f3e72c32817182
Lorran <lorrandias150@gmail.com>
2023-01-04 19:10:51 -0300

    Class 15 with exercices

---
+++ aula14/aula14.js
@@ -0,0 +1,21 @@
+//IEEE 754-2008 (Javascript imprecisions) 
+let num1 = 0.7; //Number
+let num2 = 0.1; //Number
+
+num1 += num2; // 0.8
+num1 += num2; // 0.9
+num1 += num2; // 1.0
+
+console.log(num1 + num2);
+
+
+
+//console.log(num1.toString() + num2);
+//num1 = num1.toString(); //Changes the variable type to String permanently
+//console.log(num1.toString(2));
+//console.log(typeof num1);
+//console.log(num1.toFixed(2)); //Choose how many decimal places will be returned
+//console.log(Number.isInteger(num1)); //Returns if the number is an Integer (true or false)
+//let temp = num1 * 'Ola'; //Returns a invalid operation
+//console.log(Number.isNaN(temp)); //Returns if the var is "Not a Number" (true or false)
+console.log(temp);
---
+++ aula15/Exercícios/JS/script.js
@@ -0,0 +1,16 @@
+const number = parseInt(prompt("Chose a number: "));
+const yourNumber = document.getElementById('your-number');
+const numberSquareRoot = document.getElementById('number-square-root');
+const numberIsInteger = document.getElementById('number-is-integer');
+const numberIsNaN = document.getElementById('number-is-nan');
+const numberRoundDown = document.getElementById('number-floor');
+const numberRoundUp = document.getElementById('number-ceil');
+const numberDecimalPlaces = document.getElementById('number-two-decimal-places');
+
+yourNumber.innerHTML = number;
+numberSquareRoot.innerHTML = number * 0.5;
+numberIsInteger.innerHTML = Number.isInteger(number);
+numberIsNaN.innerHTML = Number.isNaN(number);
+numberRoundDown.innerHTML = Math.floor(number);
+numberRoundUp.innerHTML = Math.ceil(number);
+numberDecimalPlaces.innerHTML = number.toFixed(2);
