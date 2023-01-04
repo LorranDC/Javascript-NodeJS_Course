d87e3b32fd140ecacd9b531f8d68993afba1ec75
Lorran Dias Carmácio <lorran@minasulcorp.com>
2023-01-04 11:23:51 -0300

    aula 14

--- aula14/index.js
+++ aula14/index.js
@@ -1,2 +1,11 @@
 let num1 = 9.54578;
-let num2 = Math.floor(num1);
+//let num2 = (50 / 0); // Return true (Infinity) 
+//let num2 = Math.pow(2 , 12); //Potenciação
+//let num2 = (2 ** 12); //Potenciação
+//let num2 = Math.floor(num1); //Arredonda pra baixo
+//let num2 = Math.ceil(num1); //Arredonda pra cima
+//let num2 = Math.round(num1); //Arredonda automaticamente
+//let num2 = Math.max(num1); //Retorna o maior número de uma sequência de números
+//let num2 = Math.min(num1); //Retorna o menor número de uma sequência de números
+//const pseudorandom = Math.random() * (10 - 5) + 5; //Gera um número pseudoaleatório (como parâmetro é possível selecionar o número máximo e o mínimo e gerar um valor entre eles)
+
