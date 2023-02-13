f683e51284c573c18a1196b8db816bcd0fd2bafd
Lorran <lorrandias150@gmail.com>
2023-02-12 22:42:59 -0300

    Exercise done

--- aula26-exercicio/modelo/assets/js/main.js
+++ aula26-exercicio/modelo/assets/js/main.js
@@ -23,7 +23,57 @@ function calculateCMI()
             setResult('Invalid Height', false);
             return;
         }
+
+        const cmi = getCMI(weight, height);
+        const cmiLevel = getCMILevel(cmi);
+        const msg = `Your CMI is ${cmi} (${cmiLevel}).`;
+
+        setResult(msg, true);
     });
+
+    
+
+    function getCMI(weight, height)
+    {
+        const cmi = weight / height ** 2;
+        return cmi.toFixed(2);
+    }
+
+    function getCMILevel(cmi)
+    {
+        const level = [ 'Underweight',
+                        'Normal weight',
+                        'Overweight', 
+                        'Obesity grade 1', 
+                        'Obesity grade 2', 
+                        'Obesity grade 3'];
+        
+        if(cmi >= 39.9)
+        {
+            return level[5];
+        }
+
+        if(cmi >= 34.9)
+        {
+            return level[4];
+        }
+
+        if(cmi >= 29.9)
+        {
+            return level[3];
+        }
+
+        if(cmi >= 24.9)
+        {
+            return level[2];
+        }
+
+        if(cmi >= 18.5)
+        {
+            return level[1];
+        }
+
+    }
 
     function createParagraph() 
     {
@@ -35,8 +85,17 @@ function calculateCMI()
     {
         const result = document.querySelector('#result');
         result.innerHTML = '';
-        result.appendChild(p);
         const p = createParagraph();
+        
+        if (isValid) 
+        {
+            p.classList.add('paragraph-result');
+        }else{
+            p.classList.add('bad');
+        }
+
+        p.innerHTML = msg;
+        result.appendChild(p);
     }
 
 
