06350fd91502c6876c09a626075c2567076f4d50
LorranDC <lorrandias150@gmail.com>
2024-03-31 01:39:02 -0300

    Webpack modules loading css

--- webpack-boilerplate-import/src/index.js
+++ webpack-boilerplate-import/src/index.js
@@ -1,4 +1,5 @@
-// 1
-// 2
-// 3
-console.log('EIIIIIIIIIIIIIIIIIIIIIII');
+import multiplication, {name, lastName, age, sum} from './modulo1';
+
+console.log(multiplication(5,26));
+console.log(name, lastName, age, sum(4,4));
+
---
+++ webpack-boilerplate-import/src/modulo1.js
@@ -0,0 +1,10 @@
+export const name = 'Lorran';
+export const lastName = 'Dias';
+export const age = 20;
+const cpf = 'asdasaf';
+
+function sum(x,y){
+    return x + y;
+}
+
+export {name, lastName, age, sum };
