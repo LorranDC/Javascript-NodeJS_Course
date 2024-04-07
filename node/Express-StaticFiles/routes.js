5d7fc1806a9256990aad984884221d757c5adb0e
LorranDC <lorrandias150@gmail.com>
2024-04-06 22:06:00 -0300

    Express static files

---
+++ node/Express-StaticFiles/routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const route = express.Router();
+const homeController = require('./src/controllers/homeController');
+const contactController = require('./src/controllers/contactController');
+
+//Home Routes
+route.get('/', homeController.homePage);
+route.post('/', homeController.treatsPost);
+
+route.get('/contact', contactController.homePage);
+
+
+
+module.exports = route;
+
