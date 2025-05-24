c916c8916b0baeb509a5e27b39ae534628179436
LorranDC <lorrandias150@gmail.com>
2025-05-24 16:32:56 -0300

    Configurando e testando o Sequelize, criando usuarios e CRUD de usuarios

--- API_REST/app.js
+++ API_REST/app.js
@@ -1,5 +1,12 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import './src/database';
+
 import express from 'express';
 import homeRoutes from './src/routes/homeRoutes';
+import userRoutes from './src/routes/userRoutes';
 
 class App {
 
@@ -16,6 +23,7 @@ class App {
 
   routes() {
     this.app.use('/', homeRoutes);
+    this.app.use('/users', userRoutes)
   }
 
 }
--- API_REST/src/controllers/HomeController.js
+++ API_REST/src/controllers/HomeController.js
@@ -1,9 +1,18 @@
-class HomeController {
-  index(req, res) {
-    res.json({
-        "tudoCerto": true
+import Aluno from '../models/Aluno';
 
-  });
+class HomeController {
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Lorran',
+      sobrenome: 'Dias',
+      email: 'lorrandias150@gmail.com',
+      idade: 21,
+      peso: 75,
+      altura: 1.68,
+
+    });
+
+    res.json(novoAluno);
   }
 
 
