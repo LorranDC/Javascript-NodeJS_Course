f18d9dfe48a36409501335cb4a37d5a15098c72b
LorranDC <lorrandias150@gmail.com>
2025-05-25 23:53:24 -0300

    arquivos estáticos

--- API_REST/src/controllers/AlunoController.js
+++ API_REST/src/controllers/AlunoController.js
@@ -9,7 +9,7 @@ class AlunoController {
       order: [['id', 'DESC'], [Foto, 'id', 'DESC']],
       include: {
         model: Foto,
-        attributes: ['filename'],
+        attributes: ['url', 'filename'],
       },
     });
     res.json(alunos);
@@ -48,7 +48,7 @@ class AlunoController {
         order: [['id', 'DESC'], [Foto, 'id', 'DESC']],
         include: {
           model: Foto,
-          attributes: ['filename'],
+          attributes: ['url', 'filename'],
         },
       });
 
--- API_REST/src/models/Foto.js
+++ API_REST/src/models/Foto.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from "sequelize";
+import appConfig from "../config/appConfig";
 
 
 export default class Foto extends Model {
@@ -22,6 +23,12 @@ export default class Foto extends Model {
           }
         }
       },
+      url: {
+        type: Sequelize.VIRTUAL,
+        get() {
+          return `${appConfig.url}/images/${this.getDataValue('filename')}`
+        }
+      }
     }, {
       sequelize,
       tableName: 'fotos',
