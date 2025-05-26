0ef9705949f7d680ea2c9d13a0159ca6dfc234ae
LorranDC <lorrandias150@gmail.com>
2025-05-25 23:11:38 -0300

    Salvando foto na base de dados

--- API_REST/src/routes/fotoRoutes.js
+++ API_REST/src/routes/fotoRoutes.js
@@ -1,15 +1,13 @@
 import { Router } from 'express';
-import multer from 'multer';
+import loginRequired from '../middlewares/loginRequired';
 
 import fotoController from '../controllers/FotoController';
-import multerConfig from '../config/multerConfig';
 
-const upload = multer(multerConfig);
 
 const router = new Router();
 
 
-router.post('/', upload.single('foto'), fotoController.store)
+router.post('/', loginRequired, fotoController.store)
 
 
 export default router;
