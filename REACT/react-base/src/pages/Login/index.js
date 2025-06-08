e18144f16da82120a191dab6449d7a1cc55e8616
LorranDC <lorrandias150@gmail.com>
2025-06-08 19:56:43 -0300

    Redux Saga

--- REACT/react-base/src/pages/Login/index.js
+++ REACT/react-base/src/pages/Login/index.js
@@ -12,7 +12,7 @@ export default function Login() {
     function handleClick(e) {
         e.preventDefault();
 
-        dispatch(exampleActions.clicaBotao());
+        dispatch(exampleActions.clicaBotaoRequest());
     }
 
     return (
