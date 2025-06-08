c94e6ac9015016d325dda62eac1d5fe97a97b2fa
LorranDC <lorrandias150@gmail.com>
2025-06-08 00:33:35 -0300

    Configurando Axios

--- REACT/react-base/src/pages/Login/index.js
+++ REACT/react-base/src/pages/Login/index.js
@@ -4,7 +4,6 @@ import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragrafo } from './styled';
 
 export default function Login() {
-
     return (
         <Container>
             <Title>
---
+++ REACT/react-base/src/services/axios.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL:'https://apicursojs.lorrandias.com.br',
+});
