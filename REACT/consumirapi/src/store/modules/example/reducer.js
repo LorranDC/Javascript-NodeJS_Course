b0f210edccc08844f8497fae03610eae1a4662f8
LorranDC <lorrandias150@gmail.com>
2025-06-08 22:01:21 -0300

    Criada a pasta do projeto real

---
+++ REACT/consumirapi/src/store/modules/example/reducer.js
@@ -0,0 +1,31 @@
+import * as types from '../types'
+
+const initialState = {
+    botaoClicado: false,
+};
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        case types.BOTAO_CLICADO_SUCCESS:{
+            console.log('Sucesso!');
+            const newState = { ...state };
+            newState.botaoClicado = !newState.botaoClicado;
+            return newState;
+        }
+
+        case types.BOTAO_CLICADO_FAILURE:{
+            const newState = { ...state };
+            console.log('Deu erro!');
+            return state;
+        }
+
+        case types.BOTAO_CLICADO_REQUEST:{
+            console.log('Estou fazendo a requisição!');
+            return state;
+        }
+
+        default: {
+            return state;
+        }
+    }
+}
---
+++ REACT/consumirapi/src/store/modules/rootReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+
+import exampleReducer from './example/reducer';
+
+export default combineReducers({
+    example: exampleReducer,
+
+});
