53601c806112c7b9d9d12c7a4fe2273a9295c859
LorranDC <lorrandias150@gmail.com>
2025-06-11 17:16:05 -0300

    Edição de dados do usuário logado - Parte 1

--- REACT/consumirapi/src/store/modules/auth/actions.js
+++ REACT/consumirapi/src/store/modules/auth/actions.js
@@ -18,3 +18,11 @@ export function loginFailure(payload) {
         payload,
     }
 }
+
+
+export function registerRequest(payload) {
+    return {
+        type: types.REGISTER_REQUEST,
+        payload,
+    }
+}
--- REACT/consumirapi/src/store/modules/auth/sagas.js
+++ REACT/consumirapi/src/store/modules/auth/sagas.js
@@ -1,4 +1,4 @@
-import { call, put, all, takeLatest} from 'redux-saga/effects';
+import { call, put, all, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 import * as actions from './actions';
 import * as types from '../types';
@@ -9,35 +9,44 @@ import { get } from 'lodash';
 
 
 
-function* loginRequest({payload}) {
-    
+function* loginRequest({ payload }) {
+
     try {
         const response = yield call(axios.post, '/tokens', payload);
-        yield put(actions.loginSuccess({...response.data}));
+        yield put(actions.loginSuccess({ ...response.data }));
 
         toast.success('Login feito com sucesso!');
 
         axios.defaults.headers.Authorization = `Bearer ${response.data.token}`;
 
         history.push(payload.prevPath);
-        
+
     } catch (e) {
         toast.error('Usuário ou senha inválidos!');
 
         yield put(actions.loginFailure());
     }
-  
+
 }
 
-function persistRehydrate({payload}) {
-        const token = get(payload, 'auth.token', '');
-        if(!token) return;
-        axios.defaults.headers.Authorization = `Bearer ${token}`;
-    }
+function persistRehydrate({ payload }) {
+    const token = get(payload, 'auth.token', '');
+    if (!token) return;
+    axios.defaults.headers.Authorization = `Bearer ${token}`;
+}
+
+function registerRequest({payload}) {
+    const {id,nome,email,password} = payload;
+    
+    
+
+}
 
 export default all([
     takeLatest(types.LOGIN_REQUEST, loginRequest),
     takeLatest(types.PERSIST_REHYDRATE, persistRehydrate),
+    takeLatest(types.REGISTER_REQUEST, registerRequest),
     
+
 
 ]);
