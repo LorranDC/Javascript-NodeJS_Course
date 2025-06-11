145f4df01ae006588b8c12bc7af09ceb1e7c7b5e
LorranDC <lorrandias150@gmail.com>
2025-06-11 19:58:16 -0300

    Logout

--- REACT/consumirapi/src/components/Header/index.js
+++ REACT/consumirapi/src/components/Header/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
+import { FaHome, FaSignInAlt, FaUserAlt, FaCircle, FaPowerOff } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
-
+import * as actions from '../../store/modules/auth/actions';
+import history from '../../services/history';
 import { Nav } from './styled';
 
 export default function Header() {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+
+  const handleLogout = e => {
+    e.preventDefault();
+    dispatch(actions.loginFailure());
+    history.push('/');
+
+  }
 
   return (
     <Nav>
@@ -17,9 +28,20 @@ export default function Header() {
         <FaUserAlt size={24} />
       </Link>
 
-      <Link to="/login">
+
+      {isLoggedIn ? (
+         <Link onClick={handleLogout} to="/logout">
+        <FaPowerOff size={24} />
+      </Link>
+      ) :(
+        <Link to="/login">
         <FaSignInAlt size={24} />
       </Link>
+      )}
+      
+
+
+      {isLoggedIn && <FaCircle size={24} color="#66ff33" />}
     </Nav>
   );
 }
