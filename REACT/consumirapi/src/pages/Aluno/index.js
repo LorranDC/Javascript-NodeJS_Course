bb0f32023c0ea8f1604c8b5953f7f58186ad10d3
LorranDC <lorrandias150@gmail.com>
2025-06-11 23:59:40 -0300

    Cadastro de alunos - Parte 3

--- REACT/consumirapi/src/pages/Aluno/index.js
+++ REACT/consumirapi/src/pages/Aluno/index.js
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import { forEach, get } from 'lodash';
-import {isEmail, isInt, isFloat} from 'validator/lib/isEmail';
+import {isEmail, isInt, isFloat} from 'validator';
 import PropTypes from 'prop-types';
 import { toast } from 'react-toastify';
+import { useDispatch } from 'react-redux';
+
 
 import axios from '../../services/axios';
 import history from '../../services/history';
 import { Container } from '../../styles/GlobalStyles';
 import {Form} from './styled';
 import Loading from '../../components/Loading';
+import * as actions from '../../store/modules/auth/actions';
 
 export default function Aluno({match}) {
+    const dispatch = useDispatch();
+
     const id = get(match, 'params.id', 0);
     const [nome, setNome] = useState('');
     const [sobrenome, setSobrenome] = useState('');
@@ -63,7 +68,7 @@ export default function Aluno({match}) {
         getData();
     },[id]);
 
-    const handleSubmit = e => {
+    const handleSubmit = async e => {
         e.preventDefault();
         let formErrors = false;
 
@@ -94,6 +99,56 @@ export default function Aluno({match}) {
             toast.error('Altura inválida');
             formErrors = true;
         }
+
+        if(formErrors) return;
+
+
+        try {
+            setIsLoading(true);
+            if(id) {
+                //editando
+                await axios.put(`/alunos/${id}`, {
+                    nome,
+                    sobrenome,
+                    email,
+                    idade,
+                    peso,
+                    altura
+                });
+
+                toast.success('Aluno(a) editado(a) com sucesso!')
+            } else {
+                //criando
+                const {data} = await axios.post(`/alunos/`, {
+                    nome,
+                    sobrenome,
+                    email,
+                    idade,
+                    peso,
+                    altura
+                });
+
+                toast.success('Aluno(a) criado(a) com sucesso!')
+                history.push(`/aluno/${data.id}/edit`);
+            }  
+
+            setIsLoading(false);
+        } catch (err) {
+            const status = get(err, 'response.status', 0)
+            const data = get(err, 'response.data', {});
+            const errors = get(data, 'errors', []);
+
+            if(errors.length > 0) {
+                errors.map(error => toast.error(error));
+            } else {
+                toast.error('Erro desconhecido');
+            }
+
+            if(status === 401) {
+                dispatch(actions.loginFailure());
+            }
+            
+        }
     }
 
 
@@ -123,7 +178,7 @@ export default function Aluno({match}) {
                 />
                 
                 <input 
-                type="text" 
+                type="number" 
                 value={idade} 
                 onChange={e => setIdade(e.target.value)} 
                 placeholder='Idade'
